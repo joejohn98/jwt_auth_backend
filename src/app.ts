259580d804082d0d16b70a6b8a8aeb9f324dc39f
joejohn98 <joejohn048@gmail.com>
2025-12-30 12:59:06 +0530

    feat: set up Express server with middleware and initial route
    - Added Express, CORS, Morgan, dotenv, and Mongoose as dependencies in package.json.
    - Created app.ts to configure the Express server with CORS, logging, and JSON parsing.
    - Implemented a basic GET route to confirm the API is running.
    - Initialized dotenv in index.ts to manage environment variables.

---
+++ src/app.ts
@@ -0,0 +1,17 @@
+import express from 'express';
+import cors from 'cors';
+import morgan from 'morgan';
+
+const app = express();
+
+
+app.use(cors())
+app.use(morgan('dev'));
+app.use(express.json());
+
+
+app.get('/', (req, res)=>{
+    res.send("API is running...!")
+})
+
+export default app;
