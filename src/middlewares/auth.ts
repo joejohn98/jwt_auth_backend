4115a12a65327c2801862d061c5862ad9ccb8e78
joejohn98 <joejohn048@gmail.com>
2025-12-30 20:54:47 +0530

    add middleware to protect routes with JWT authentication

---
+++ src/middlewares/auth.ts
@@ -0,0 +1,29 @@
+import type { Request, Response, NextFunction } from "express";
+
+import { verifyToken } from "../utils/jwt";
+
+const protect = (req: Request, res: Response, next: NextFunction) => {
+  const authHeader = req.headers.authorization;
+
+  if (!authHeader || !authHeader.startsWith("Bearer ")) {
+    res.status(401).json({
+      status: "failed",
+      message: "you are not authorized",
+    });
+    return;
+  }
+
+  try {
+    const token = authHeader.split(" ")[1];
+    verifyToken(token as string);
+    next();
+  } catch (error) {
+    console.log("error to protect", error);
+    res.status(500).json({
+      status: "failed",
+      message: "Invalid or expired token",
+    });
+  }
+};
+
+export default protect;
